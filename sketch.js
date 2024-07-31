0a2620e9259cc778ba4cbb3f02afc48dee902387
Isabelle <0000112585246xsp@al.educacao.sp.gov.br>
2024-07-31 14:53:34 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1000, 800);
+  background("#8C0EA2");
+}
+
+function draw() {
+  
+  
+  fill("red");
+  stroke("black");
+  
+  if (mouseIsPressed);{
+  circle(mouseX,mouseY,30);
+  }
+}
